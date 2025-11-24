daa715ea207355281afae2b6c77fb36c8c72c8d3
Mohit <mohitt.tech@gmail.com>
2025-11-24 22:03:30 +0530

    Create top_10_highest_revenue.py

---
+++ Filtering & Sorting/top_10_highest_revenue.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load CSV
+df = pd.read_csv('data/tmdb_5000_movies.csv')
+
+# Fill missing revenue with 0 (optional, if needed)
+df['revenue'] = df['revenue'].fillna(0)
+
+# Sorting revenue descending and get top 10
+top_10_revenue = df.sort_values('revenue', ascending=False).head(10)
+
+# Display movie title and revenue
+print("Top 10 highest revenue movies :\n",top_10_revenue[['title', 'revenue']])
