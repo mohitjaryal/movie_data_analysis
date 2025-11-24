bce8ce8cf03cf2c2407e6d935441cff764455a9a
Mohit <mohitt.tech@gmail.com>
2025-11-24 23:53:41 +0530

    Update avg_rating_per_year.py

--- Grouping&Aggregation/avg_rating_per_year.py
+++ Grouping&Aggregation/avg_rating_per_year.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Load CSV
-df = pd.read_csv('/data/tmdb_5000_movies.csv')
+df = pd.read_csv('data/tmdb_5000_movies.csv')
 
 # Convert release_date to datetime
 df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
