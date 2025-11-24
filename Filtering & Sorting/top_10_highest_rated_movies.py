c7b9fd5301a87b75c0e3636bcd5c4ca3b0bf1402
Mohit <mohitt.tech@gmail.com>
2025-11-24 22:11:10 +0530

    Update top_10_highest_rated_movies.py

--- Filtering & Sorting/top_10_highest_rated_movies.py
+++ Filtering & Sorting/top_10_highest_rated_movies.py
@@ -5,3 +5,12 @@ df = pd.read_csv('data/tmdb_5000_movies.csv')
 
 # Filling the missing values
 df['vote_average'] = df['vote_average'].fillna(df['vote_average']).mean()
+df['vote_count'] = df['vote_count'].fillna(df['vote_count']).mean()
+
+# min vote count to filter
+min_votes = 50
+popular_movies = df[df['vote_count'] >= min_votes]
+
+# sorting to get top 10 movies
+top_10_movies = popular_movies.sort_values('vote_average',ascending=False).head(10)
+print('Top 10 popular movies :\n',top_10_rated[['title', 'vote_average', 'vote_count'])
