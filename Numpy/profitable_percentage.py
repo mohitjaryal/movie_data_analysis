3262851dc828e12e964c171048149c2841c7e661
Mohit <mohitt.tech@gmail.com>
2025-11-25 00:09:15 +0530

    Create profitable_percentage.py

---
+++ Numpy/profitable_percentage.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+# Load dataset
+df = pd.read_csv('data/tmdb_5000_movies.csv')
+
+# data cleaning
+df['budget'] = pd.to_numeric(df['budget'], errors='coerce').fillna(df['budget'].mean())
+df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce').fillna(df['revenue'].mean())
+
+# calculating profit
+df['revenue'].to_numpy - df['budget'].to_numpy() 
