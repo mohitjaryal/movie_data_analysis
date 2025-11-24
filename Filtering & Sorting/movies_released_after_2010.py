4cfd8019d901a4b95abfb0ffb4956663b7fa84c8
Mohit <mohitt.tech@gmail.com>
2025-11-24 22:20:42 +0530

    Update movies_released_after_2010.py

--- Filtering & Sorting/movies_released_after_2010.py
+++ Filtering & Sorting/movies_released_after_2010.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 # Load CSV
 df = pd.read_csv('data/tmdb_5000_movies.csv')
+
+# converting releasedate to datetime
+df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+
+# filtering movies after 2010
+movies_after_2010 = df[df['release_date'].dt.year > 2010]
+
+# displaying results
+print('Movies released after 2010 : \n',movies_after_2010[['title','release_date']])
+
+# total no. of movies releaded 
