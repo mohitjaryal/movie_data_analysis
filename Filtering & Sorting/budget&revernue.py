8604482d6a3e53f38daf15dadb41ffa15c7958bc
Mohit <mohitt.tech@gmail.com>
2025-11-24 23:42:33 +0530

    Update budget&revernue.py

--- Filtering & Sorting/budget&revernue.py
+++ Filtering & Sorting/budget&revernue.py
@@ -8,3 +8,8 @@ df = pd.read_csv('data/tmdb_5000_movies.csv')
 #filling missing numeric values with mean()
 df['budget'] = df['budget'].fillna(df['budget'].mean())
 df['revenue'] = df['revenue'].fillna(df['budget'].mean())
+
+
+# Convert to numeric 
+df['budget'] = pd.to_numeric(df['budget'], errors='coerce')
+df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce')
