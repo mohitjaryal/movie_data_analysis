f8d083c6968d51586bb098e46c6b224ead5b8dc0
Mohit <mohitt.tech@gmail.com>
2025-11-25 21:05:08 +0530

    Update profit_calc.py

---
+++ Numpy/profit_calc.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# Load dataset
+df = pd.read_csv('data/tmdb_5000_movies.csv')
+
+# data cleaning
+df['budget'] = pd.to_numeric(df['budget'], errors='coerce').fillna(df['budget'].mean())
+df['revenue'] = pd.to_numeric(df['revenue'], errors='coerce').fillna(df['revenue'].mean())
+
+# calculating profit
+profit = df['revenue'].to_numpy() - df['budget'].to_numpy()
+
+print('Profit :\n',profit)
+
+# few examples
+print("Example of profit calculation:\n", df[['title', 'budget', 'revenue']].head())
